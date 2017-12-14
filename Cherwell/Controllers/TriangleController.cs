756725106aee11f7ed51aa1a6b145407cccbe4c6
Charles Dollery <chuck.dollery@outlook.com>
2017-12-14 00:08:36 -0500

    initial upload

---
+++ Cherwell/Controllers/TriangleController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cherwell.Controllers
+{
+	/// <summary>
+	/// Controller to process the triangle
+	/// </summary>
+	[Produces("application/json")]
+	[Route("api/Triangle")]
+	public class TriangleController : Controller
+	{
+		/// <summary>
+		/// Gets all coordinates for the triangles
+		/// </summary>
+		/// <returns></returns>
+		// GET: api/Triangle
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var coordinates = Models.Triangle.GetAllCoordinates();
+			var onlyCoordinates = coordinates.Select(child => child.Item3).ToList();
+
+			return Ok(onlyCoordinates);
+		}
+
+		/// <summary>
+		/// Gets a single coordinate based on the <paramref name="row"/> and <paramref name="row"/> supplied
+		/// </summary>
+		/// <param name="row">The row to fetch the coordinate for. <see cref="Models.Rows"/></param>
+		/// <param name="column">The column to fetch the coordinate for, max value is <see cref="Models.Triangle.MaxColumn"/></param>
+		/// <returns></returns>
+		// GET: api/Triangle/A/1
+		[HttpGet("{row}/{column}", Name = "Get")]
+		public IActionResult Get(Models.Rows row, int column)
+		{
+			var triangle = new Models.Triangle(row, column);
+
+			if (triangle.IsValid)
+				return Ok(triangle.Coordinates);
+			else
+				return BadRequest("Validation error");
+		}
+
+		/// <summary>
+		/// Gets the row <see cref="Models.Rows" /> and column /> for the triangle for the given coordinates
+		/// </summary>
+		/// <param name="V1X">X part for vertex 1</param>
+		/// <param name="V1Y">Y part for vertex 1</param>
+		/// <param name="V2X">X part for vertex 2</param>
+		/// <param name="V2Y">Y part for vertex 2</param>
+		/// <param name="V3X">X part for vertex 3</param>
+		/// <param name="V3Y">Y part for vertex 3</param>
+		/// <returns></returns>
+		// GET: api/Triangle/A/1
+		[HttpGet("{V1X}/{V1Y}/{V2X}/{V2Y}/{V3X}/{V3Y}", Name = "GetRowColumn")]
+		public IActionResult Get(int V1X, int V1Y, int V2X, int V2Y, int V3X, int V3Y)
+		{
+			var triangle = new Models.Triangle(new Point(V1X, V1Y), new Point(V2X, V2Y), new Point(V3X, V3Y));
+
+			if (triangle.IsValid)
+				return Ok(new { Row = triangle.Row.ToString(), Column = triangle.Column });
+			else
+				return BadRequest("Validation error");
+		}
+	}
+}
---
+++ Cherwell/Models/Triangle.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cherwell.Models
+{
+	/// <summary>
+	/// Enum to specify the rows. "None" is so the pipeline doesn't put "A" for an out of bound value.
+	/// </summary>
+	public enum Rows
+	{
+		None, A, B, C, D, E, F
+	}
+
+	/// <summary>
+	/// Structure to store the three vertexes for the triangle
+	/// </summary>
+	public struct Coordinates
+	{
+		public Point V1 { get; private set; }
+		public Point V2 { get; private set; }
+		public Point V3 { get; private set; }
+
+		public Coordinates(Point p1, Point p2, Point p3)
+		{
+			V1 = p1;
+			V2 = p2;
+			V3 = p3;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Coordinates))
+				return false;
+
+			if (obj == null)
+				return false;
+
+			return (this.V1 == ((Coordinates)obj).V1) &&
+				   (this.V2 == ((Coordinates)obj).V2) &&
+				   (this.V3 == ((Coordinates)obj).V3);
+		}
+
+		public override int GetHashCode()
+		{
+			return V1.GetHashCode() ^ V2.GetHashCode() ^ V3.GetHashCode();
+		}
+
+		public static bool operator ==(Coordinates coord1, Coordinates coord2)
+		{
+			return coord1.Equals(coord2);
+		}
+
+		public static bool operator !=(Coordinates coord1, Coordinates coord2)
+		{
+			return !coord1.Equals(coord2);
+		}
+	}
+
+	/// <summary>
+	/// Class to validate and calculate the vertexes and Row/Column
+	/// </summary>
+	public class Triangle
+	{
+		internal const int MaxColumn = 12;
+
+		public Rows Row { get; private set; }
+		public int Column { get; private set; }
+		public Coordinates Coordinates { get; private set; }
+		public bool IsValid { get; private set; }
+
+		public Triangle(Rows row, int column)
+		{
+			Row = row;
+			Column = column;
+
+			IsValid = DetermineIsValid(row, column);
+
+			if (IsValid)
+				Coordinates = DetermineCoordinates(row, column);
+		}
+
+		public Triangle(Point p1, Point p2, Point p3)
+		{
+			Coordinates = new Coordinates(p1, p2, p3);
+			IsValid = false;
+
+			foreach(var coordinate in GetAllCoordinates())
+			{
+				if (Coordinates == coordinate.Item3)
+				{
+					IsValid = true;
+					Row = coordinate.Item1;
+					Column = coordinate.Item2;
+
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Validates that row and column fall in the valid range
+		/// </summary>
+		/// <param name="row"></param>
+		/// <param name="column"></param>
+		/// <returns></returns>
+		private bool DetermineIsValid(Rows row, int column)
+		{
+			if ((row >= Rows.A) && (row <= Rows.F) && (column >= 1) && (column <= MaxColumn))
+				return true;
+			else
+				return false;
+		}
+
+		private Coordinates DetermineCoordinates(Rows row, int column)
+		{
+			Point P1 = new Point();
+			Point P2 = new Point();
+			Point P3 = new Point();
+
+			int rowIndex = ((int)row) - 1;
+			int colIndex;
+			bool isEvenColumn = false;
+
+			if (column % 2 == 0)
+			{
+				isEvenColumn = true;
+				colIndex = (column - 2) / 2;
+			}
+			else
+			{
+				colIndex = (column - 1) / 2;
+			}
+
+			if (isEvenColumn)
+			{
+				P1.X = (0 + 10 * colIndex);
+				P1.Y = (0 + 10 * rowIndex);
+
+				P2.X = (10 + 10 * colIndex);
+				P2.Y = (0 + 10 * rowIndex);
+
+				P3.X = (10 + 10 * colIndex);
+				P3.Y = (10 + 10 * rowIndex);
+			}
+			else
+			{
+				P1.X = (0 + 10 * colIndex);
+				P1.Y = (10 + 10 * rowIndex);
+
+				P2.X = (0 + 10 * colIndex);
+				P2.Y = (0 + 10 * rowIndex);
+
+				P3.X = (10 + 10 * colIndex);
+				P3.Y = (10 + 10 * rowIndex);
+			}
+
+			return new Coordinates(P1, P2, P3);
+		}
+
+		internal static List<Tuple<Rows, int, Coordinates>> GetAllCoordinates()
+		{
+			var coordinates = new List<Tuple<Rows, int, Coordinates>>();
+
+			foreach (var row in Enum.GetValues(typeof(Rows)).Cast<Rows>())
+			{
+				if (row != Rows.None)
+				{
+					for (var column = 1; column <= Models.Triangle.MaxColumn; column++)
+					{
+						coordinates.Add(new Tuple<Rows, int, Coordinates>(row, column, new Models.Triangle(row, column).Coordinates));
+					}
+				}
+			}
+
+			return coordinates;
+		}
+	}
+}
